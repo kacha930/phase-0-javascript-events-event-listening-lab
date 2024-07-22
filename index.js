e5031fe5fd5322a9accb799d498bd79b5e99be2a
kacha930 <robiwere20@gmail.com>
2024-07-22 11:27:19 +0300

    event listeners

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    //because we are accessing it by its id 
+    //remember get element not elements
+    const button = document.getElementById('button');
+    button.addEventListener('click', function(){
+        alert("Button was clicked");
+    });
 }
+addingEventListener();
